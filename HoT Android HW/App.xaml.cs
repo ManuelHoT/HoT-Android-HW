2eb5d2f38f18970f975c6a2229903fc737e93753
manuel.gonzalez <manuel.gonzalez@houseoftest.se>
2022-03-10 12:38:09 +0100

    Add project files.

---
+++ HoT Android HW/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+using HoT_Android_HW.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HoT_Android_HW
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+           
+            MainPage = new AboutPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ HoT Android HW/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using HoT_Android_HW.ViewModels;
+using HoT_Android_HW.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace HoT_Android_HW
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
